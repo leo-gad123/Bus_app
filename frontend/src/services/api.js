927d4ee70529c4c50540a4a280461ad68d248111
Leogad HAKIZIMANA <hakizimanaleogad@gmail.com>
2026-06-27 18:52:19 +0200

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://smart-ebus-backend.onrender.com/api';
 const API = axios.create({ baseURL: BASE_URL });
 
 API.interceptors.request.use((config) => {
